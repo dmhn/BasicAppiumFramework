33f22b252523937fdbb354b9bb7c7cc883d6568d
Dmhn <deepmoha@yahoo.com>
2018-04-08 22:43:41 -0400

    Fist Commit of Appium framework

---
+++ src/test/java/AppiumTests/AppiumTest1.java
@@ -0,0 +1,32 @@
+package AppiumTests;
+import io.appium.java_client.MobileBy;
+
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.annotations.Test;
+
+import com.appiumbookblog.qa.base.AppiumDriverBase;
+
+import io.appium.java_client.MobileBy;
+public class AppiumTest1 extends AppiumDriverBase {
+	    //Test Annotation changes any java function to TestNG test case
+	    @Test
+	    public void sampeTest(){
+	 
+	    //click on Accessibility link
+	    wait.until(ExpectedConditions.presenceOfElementLocated(MobileBy.AccessibilityId("Accessibility")));
+	    driver.findElement(MobileBy.AccessibilityId("Accessibility")).click();
+	 
+	    //click on 'Accessibility Node Querying' link
+	    wait.until(ExpectedConditions.presenceOfElementLocated(MobileBy.AccessibilityId("Accessibility Node Querying")));
+	    driver.findElement(MobileBy.AccessibilityId("Accessibility Node Querying")).click();
+	 
+	    //back
+	    driver.navigate().back();
+	 
+	    //back
+	    driver.navigate().back();
+	    }
+	 
+	}
+
+
